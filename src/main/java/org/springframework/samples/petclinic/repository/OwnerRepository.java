f38ff30e25c952e4683378e6b6c6bc49688e0df9
Gerardo Martín Roldán <gmroldanutn@gmail.com>
2018-02-12 11:46:00 -0300

    Migrate OwnerRepository to spring-data-jpa as default implementation

--- src/main/java/org/springframework/samples/petclinic/repository/OwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/OwnerRepository.java
@@ -15,10 +15,10 @@
  */
 package org.springframework.samples.petclinic.repository;
 
-import java.util.Collection;
+import java.util.List;
 
 import org.springframework.dao.DataAccessException;
-import org.springframework.samples.petclinic.model.BaseEntity;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.samples.petclinic.model.Owner;
 
 /**
@@ -31,7 +31,7 @@ import org.springframework.samples.petclinic.model.Owner;
  * @author Michael Isvy
  * @author Vitaliy Fedoriv
  */
-public interface OwnerRepository {
+public interface OwnerRepository extends CrudRepository<Owner, Integer> {
 
     /**
      * Retrieve <code>Owner</code>s from the data store by last name, returning all owners whose last name <i>starts</i>
@@ -41,41 +41,15 @@ public interface OwnerRepository {
      * @return a <code>Collection</code> of matching <code>Owner</code>s (or an empty <code>Collection</code> if none
      * found)
      */
-    Collection<Owner> findByLastName(String lastName) throws DataAccessException;
+    List<Owner> findByLastName(String lastName) throws DataAccessException;
 
     /**
-     * Retrieve an <code>Owner</code> from the data store by id.
-     *
-     * @param id the id to search for
-     * @return the <code>Owner</code> if found
-     * @throws org.springframework.dao.DataRetrievalFailureException if not found
-     */
-    Owner findById(int id) throws DataAccessException;
-
-
-    /**
-     * Save an <code>Owner</code> to the data store, either inserting or updating it.
-     *
-     * @param owner the <code>Owner</code> to save
-     * @see BaseEntity#isNew
-     */
-    void save(Owner owner) throws DataAccessException;
-    
-    /**
-     * Retrieve <code>Owner</code>s from the data store, returning all owners 
+     * Retrieve <code>Owner</code>s from the data store, returning all owners
      *
      * @return a <code>Collection</code> of <code>Owner</code>s (or an empty <code>Collection</code> if none
      * found)
      */
-	Collection<Owner> findAll() throws DataAccessException;
-	
-    /**
-     * Delete an <code>Owner</code> to the data store by <code>Owner</code>.
-     *
-     * @param owner the <code>Owner</code> to delete
-     * 
-     */
-	void delete(Owner owner) throws DataAccessException;
-
+    @Override
+    List<Owner> findAll() throws DataAccessException;
 
 }
--- src/main/java/org/springframework/samples/petclinic/service/ClinicServiceImpl.java
+++ src/main/java/org/springframework/samples/petclinic/service/ClinicServiceImpl.java
@@ -67,7 +67,7 @@ public class ClinicServiceImpl implements ClinicService {
         this.vetRepository = vetRepository;
         this.ownerRepository = ownerRepository;
         this.visitRepository = visitRepository;
-        this.specialtyRepository = specialtyRepository; 
+        this.specialtyRepository = specialtyRepository;
 		this.petTypeRepository = petTypeRepository;
     }
 
@@ -224,7 +224,7 @@ public class ClinicServiceImpl implements ClinicService {
 	public Owner findOwnerById(int id) throws DataAccessException {
 		Owner owner = null;
 		try {
-			owner = ownerRepository.findById(id);
+			owner = ownerRepository.findOne(id);
 		} catch (ObjectRetrievalFailureException|EmptyResultDataAccessException e) {
 		// just ignore not found exceptions for Jdbc/Jpa realization
 			return null;
@@ -249,14 +249,14 @@ public class ClinicServiceImpl implements ClinicService {
 	@Transactional
 	public void savePet(Pet pet) throws DataAccessException {
 		petRepository.save(pet);
-		
+
 	}
 
 	@Override
 	@Transactional
 	public void saveVisit(Visit visit) throws DataAccessException {
 		visitRepository.save(visit);
-		
+
 	}
 
 	@Override
@@ -270,7 +270,7 @@ public class ClinicServiceImpl implements ClinicService {
 	@Transactional
 	public void saveOwner(Owner owner) throws DataAccessException {
 		ownerRepository.save(owner);
-		
+
 	}
 
 	@Override
@@ -284,8 +284,8 @@ public class ClinicServiceImpl implements ClinicService {
 	public Collection<Visit> findVisitsByPetId(int petId) {
 		return visitRepository.findByPetId(petId);
 	}
-	
-	
+
+
 
 
 }
